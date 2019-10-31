ccc6a615643bf2a23602a02088e880d970026a7f
DimejiAre <olardi2@gmail.com>
2019-10-31 12:06:24 +0100

    add fetch request

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [friends,setFriends] = useState([]);
+
+  useEffect(()=>{
+    axios.get('api/friends')
+      .then(res => {
+        setFriends(res.data)
+      })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {friends?
+      friends.map(friend => (
+        <div>Name: {friend.name} id: {friend.id}</div>
+      ))
+    : null}
     </div>
   );
 }
