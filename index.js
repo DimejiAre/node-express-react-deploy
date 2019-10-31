26d8f39675c706b124906491cf7961c94f9df805
DimejiAre <olardi2@gmail.com>
2019-10-31 10:21:20 +0100

    setup initial server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const friends = [
+    {id: 1, name: 'Dimeji'},
+    {id: 2, name: 'Ifiok'},
+    {id: 3, name: 'Brenda'}
+]
+
+app.get('/api/friends', (req,res,next) => {
+    res.json(friends);
+})
+app.listen(process.env.PORT || port, () => {
+    console.log(`listening on http://localhost:${(process.env.PORT || port)}`)
+} )
